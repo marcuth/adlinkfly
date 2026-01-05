11f93d98eb4d74a5a33d0c24bf3e436216abf6b1
Marcuth <thiago.playgames15@gmail.com>
2026-01-05 17:47:39 -0300

    feat: Introduce `BaseUrl` enum for URL shortener services and update package keywords.

---
+++ src/enums/base-url.ts
@@ -0,0 +1,12 @@
+export enum BaseUrl {
+    ShrtFly = "https://shrtfly.com/api/",
+    ExeIo = "https://exe.io/api/",
+    ClicksFly = "https://clicksfly.com/api/",
+    EncurtaNet = "https://encurtanet.com/api/",
+    ShrinkMe = "https://shrinkme.io/api/",
+    ShrinkEarn = "https://shrinkearn.com/api/",
+    ClkSh = "https://clk.sh/api/",
+    ShrtBr = "https://shrtbr.com/api/",
+    LinkMonetizado = "https://linkmonetizado.com/api/",
+    LinkAds = "https://linkads.net/api/",
+}
